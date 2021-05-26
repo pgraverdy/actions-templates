cf69b5714398b879ced64a2c5ab80c52ac317d98
pgraverdy <pierre-guillaume.raverdy@inria.fr>
2021-05-26 20:12:29 +0200

    Docker files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Entrypoint for current user id from documentation
+# <https://github.com/RHsyseng/container-rhel-examples/>
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
+exec "$@"
