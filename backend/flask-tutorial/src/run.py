4af51a1e7a0cbb883e824d41e20cbca4554789f0
pgraverdy <pierre-guillaume.raverdy@inria.fr>
2021-05-29 14:24:22 +0200

    Auto fix by local mega-linter

--- backend/flask-tutorial/src/run.py
+++ backend/flask-tutorial/src/run.py
@@ -9,7 +9,9 @@ def create_app():
     abs_file_path = os.path.abspath(os.path.dirname(__file__))
     openapi_path = os.path.join(abs_file_path, "../", "../", "openapi")
     app = connexion.FlaskApp(
-        __name__, specification_dir=openapi_path, options={"swagger_ui": False, "serve_spec": False}
+        __name__,
+        specification_dir=openapi_path,
+        options={"swagger_ui": False, "serve_spec": False},
     )
     app.add_api("spec.yml", strict_validation=True)
     flask_app = app.app
